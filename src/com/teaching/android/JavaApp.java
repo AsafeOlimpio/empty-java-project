dd00d8193ab5000de8e02cb4757ae382cd0935df
LAPTOP-DRMMMS61\asafe <asafeolimpio@gmail.com>
2018-04-17 14:08:57 +0200

    Mathematic Operations

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -8,22 +8,10 @@ public class JavaApp {
     public static void main(String[] args) {
         System.out.println("Easy!!"); // Display the string.
 
-        int i = 5; //32 bits
-        byte b = 65; //8 bits
-        char c = 'A'; //16 bits
-        boolean bol = true; //1 bit
-        short s = 65; //16 bits
-        long l = 65L; //64 bits
-        float f = 65f; //32 bits
-        double d = 65.55; //64 bits
-
-        System.out.println(i);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(bol);
-        System.out.println(s);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
+        System.out.println(2+5-3*2); //1
+        System.out.println(2*4*2/2); //8
+        System.out.println(1/2); //0
+        System.out.println(5*(1/2)); //0
+        System.out.println(5f*1/2); //2.5
     }
 }
