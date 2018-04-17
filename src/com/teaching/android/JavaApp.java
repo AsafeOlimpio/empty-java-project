69e902b4a530be7382b697f59c9f62c567561549
LAPTOP-DRMMMS61\asafe <asafeolimpio@gmail.com>
2018-04-17 13:06:53 +0200

    My First Commit

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,24 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
+        System.out.println("Easy!!"); // Display the string.
 
+        int i = 5; //32 bits
+        byte b = 65; //8 bits
+        char c = 'A'; //16 bits
+        boolean bol = true; //1 bit
+        short s = 65; //16 bits
+        long l = 65L; //64 bits
+        float f = 65f; //32 bits
+        double d = 65.55; //64 bits
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bol);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
 }
