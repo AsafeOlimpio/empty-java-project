be3beeaa54d9d28820fcf4617c213ca290b53e58
LAPTOP-DRMMMS61\asafe <asafeolimpio@gmail.com>
2018-04-19 12:57:28 +0200

    Practica Distancia Entre Puntos

--- src/com/teaching/android/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/com/teaching/android/JavaApp.java
@@ -8,7 +8,12 @@ import static com.teaching.android.GuessTheNumber.tryAgain;
 public class JavaApp extends GuessTheNumber{
 
     public static void main(String[] args) {
-
+    Punto p1 = new Punto(2,4);
+    Punto p2 = new Punto(3,5);
+    // 3 Maneras de calcular la distancia.
+    //System.out.println(p1.distancia(3,5));//Ahora ya no podemos referenciar distancia pues lo hemos cambiado a privado
+    //System.out.println("La distancia entre punto los puntos es: " + p2.distancia(2,4));
+    System.out.println("La distancia entre los puntos es: " + p2.distancia(p1));
+    System.out.println(p1);
     }
-
 }
--- src/com/teaching/android/com/teaching/android/Punto.java
+++ src/com/teaching/android/com/teaching/android/Punto.java
@@ -6,31 +6,13 @@ public class Punto {
     private int x;
     private int y;
 
-    // Methods
-
-    public int distancia (int xl, int yl){
-       // variables x
-       int x1 = 0;
-       int x2 = 0;
-       int restax = x1 - x2;
-       //variables y
-        int y1 = 0;
-        int y2 = 0;
-        int restay = y1 - y2;
-       //Formula
-       xl = restax;
-       yl = restay;
-
-
-    }
-
     //Constructors
     public Punto(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public Punto(){
+    public Punto() {
 
     }
 
@@ -49,6 +31,37 @@ public class Punto {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    // Methods
+
+    @Override
+    public String toString() {
+        return "Punto{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    private double distancia(int px, int py) {
+        // variables x
+        int difx = px - x;
+        //variables y
+        int dify = py - y;
+        //Elevación
+        double elevationx = Math.pow(difx, 2);
+        double elevationy = Math.pow(dify, 2);
+        double sumxy = elevationx + elevationy;
+        double d = Math.sqrt(sumxy);
+        return d;
+
+        //double resultado = Math.sqrt(Math.pow());
+    }
+
+    //Al crear este método puedo dejar el metodo distancia como privado
+    public double distancia(Punto p) {
+        double d = distancia(p.x,p.y);
+        return d;
     }
 
 }
